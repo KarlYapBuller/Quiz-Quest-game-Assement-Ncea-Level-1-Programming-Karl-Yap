ace9bcdff70663fffa77de2fd50397ce36bc4d7c
KarlYapBuller <78565635+KarlYapBuller@users.noreply.github.com>
2021-07-06 23:48:21 +1200

    The question was added in the unction and in the response

--- 03_How_many_questions_v3.py
+++ 03_How_many_questions_v3.py
@@ -3,15 +3,15 @@
 #User can choose how many questions they want to answer with the minimum number of questions the
 #User has to input they want to answer being 1 and there is
 #no maximum number for how many questions they want to answer and the other option
-#the User can choose is the Continuous questions where the User will be asked the continuous number of questions. 
+#the User can choose is the Continuous questions where the User will be asked the continuous number of questions.
 
 
-def check_how_many_questions():
+def check_how_many_questions(question):
     while True:
         #Ask the User how many questions they want to answer if they want to play the
         #option where they can choose how many questions they want
         #to answer of if they want to play the continuous question option
-        response = input("How many Questions would you like to answer (Press <ENTER> for continous question)? ")
+        response = input(question)
 
         how_many_questions_error = "Please input either an integer that is more than 0 or <ENTER>."
 #If ifinite mode is not chosen, check response is an integer more than 0
@@ -34,7 +34,7 @@ def check_how_many_questions():
 
 number_of_questions_answered = 0
 
-user_choice_of_questions = check_how_many_questions()
+user_choice_of_questions = check_how_many_questions("How many Questions would you like to answer (Press <ENTER> for continous question)? ")
 
 #If the User's Choice is <ENTER> the User has decided to answer the Continuous Question's option
 if user_choice_of_questions == "":
