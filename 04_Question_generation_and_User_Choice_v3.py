97adc3b1fc17a687c9e3d6bbaafaa8d6cc7fa571
KarlYapBuller <78565635+KarlYapBuller@users.noreply.github.com>
2021-07-09 01:46:11 +1200

    Put the question in the function

--- 04_Question_generation_and_User_Choice_v3.py
+++ 04_Question_generation_and_User_Choice_v3.py
@@ -1,1 +1,35 @@
 #Question generation and User Choice component version 3
+
+import random
+
+#Number of question answered
+number_of_questions_answered = 0
+
+for item in range(0,10):
+
+    #Prints out the Question Number
+    print("*****Question {}*****".format(number_of_questions_answered +1))
+
+    number_1 = random.randint(0,20)
+    number_2 = random.randint(0,20)
+    answer = number_1 + number_2
+
+    error_message = "<ERROR> Please input an Integer"
+
+    try:
+        response = float(input("What is {} + {}?  ".format(number_1, number_2)))
+
+        if response == answer:
+            result = "Correct"
+            print("Result: {} | Your Answer: {} | Correct Answer: {}".format(result, response, answer))
+            print()
+            number_of_questions_answered += 1
+
+        else:
+            result = "Incorrect"
+            print("Result: {} | Your Answer: {} | Correct Answer: {}".format(result, response, answer))
+            print()
+            number_of_questions_answered += 1
+
+    except ValueError:
+        print(error_message)
