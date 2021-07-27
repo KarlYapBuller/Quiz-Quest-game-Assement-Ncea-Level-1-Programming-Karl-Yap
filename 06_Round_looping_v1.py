0bf69c897b05ab66acce38411b6d518d7403ff14
KarlYapBuller <78565635+KarlYapBuller@users.noreply.github.com>
2021-07-27 12:06:38 +1200

    There is a problem with the number of questions answered not reseting

--- 06_Round_looping_v1.py
+++ 06_Round_looping_v1.py
@@ -4,33 +4,39 @@
 #They press <ENTER> to play the game again,
 #The round heading (the number of questions the User answered) does not reset
 
+#The import random is used for the random integer between the specified ranges
 import random
 
+#Functions
+
+#Function to check how many questions the User wants to play
+#User has a choice of how many questions they want to answer or they can choose the
+#Continous question option
 def check_how_many_questions(question):
     while True:
-        #Ask the User how many questions they want to answer if they want to play the
-        #option where they can choose how many questions they want
-        #to answer of if they want to play the continuous question option
+        #The User will input a response to the given question
         response = input(question)
 
+        #Error message
         how_many_questions_error = "Please input either an integer that is more than 0 or <ENTER>."
-#If ifinite mode is not chosen, check response is an integer more than 0
+        #If ifinite mode is not chosen, check response is an integer more than 0
         if response != "":
             try:
                 response = int(response)
 
-#If response is too low, go back to the start of the loop and display an error message to help user
+                #If response is too low, go back to the start of the loop and display an error message to help user
                 if response < 1:
                     print(how_many_questions_error)
                     continue
 
+            #If the User inputs an invalid value, Error message is displayed
             except ValueError:
                 print(how_many_questions_error)
                 continue
 
         return response
 
-#Game Mode Input Checker Function
+#Function to check what Game Mode the User wants to play
 def game_mode_input_checker(question):
     while True:
 
@@ -41,37 +47,39 @@ def game_mode_input_checker(question):
             #It is an float input in the case the User inpurs a valid input but just with a .0
             response = float(input(question))
 
-            #If User's response is 1 return the response
+            #If User's response is 1 (Addition) return the response
             if response == 1:
                 return response
 
-            #If User's response is 2 return the response
+            #If User's response is 2 (Subtraction) return the response
             elif response == 2:
                 return response
 
-            #If User's response is 3 return the response
+            #If User's response is 3 (Multiplication) return the response
             elif response == 3:
                 return response
 
-            #If User's response is 3 return the response
+            #If User's response is 4 (Division) return the response
             elif response == 4:
                 return response
 
-            #If User's response is not 1,2,3,4 print the <ERROR> message
+            #If User's response is not 1,2,3,4 print the ERROR message
             else:
                 print(error_message)
                 print()
                 continue
 
-        #If the User inputs an invalid value print the <ERROR> message
+        #If the User inputs an invalid value print the ERROR message
         except ValueError:
             print(error_message)
             print()
 
-#Input Checker
+#Function to check the User's input
+#In this instance this function is used to check if the User's input for the Mathematical questions is valid
 def input_checker(question):
     while True:
         try:
+            #The User will input a response to the given question
             #It is an float input in the case the User inputs a calid input but just with a .0
             response = float(input(question))
             return response
@@ -81,9 +89,12 @@ def input_checker(question):
             continue
 
 #Continue the game function
+#This functions purpose is to ask the User after each question or if they quit playing the game if they wish to continue or quit playing the game, 'xxx'
 def continue_game(question):
     valid = False
     while not valid:
+        #The User will input a response to the given question
+        #.lower() is used in the case the User types the correct input but with the wrong casing
         response = input(question).lower()
 
         if response == "":
@@ -102,11 +113,17 @@ def continue_game(question):
 #Decorates the statements in the Lucky Unicorn game
 def statement_generator(statement, decoration):
 
+    #The of the statement is the chosen decoration times by 3
     sides = decoration * 3
 
+    #The statement outputed to the User is the chosen statement with
+    #the sides on the left and right of the statement
     statement = "{} {} {}".format(sides, statement, sides)
+    #The decotration on the top and bottom of the the statement is
+    #the chosen decoration and it is the length of the statement
     top_bottom = decoration * len(statement)
 
+    #top_bottom and statement variables are outputed to the User
     print(top_bottom)
     print(statement)
     print(top_bottom)
@@ -115,19 +132,28 @@ def statement_generator(statement, decoration):
 
 #Main routine goes here
 
-#Questions answered correct
-questions_answered_correct = 0
-
-#Questions answered incorrect
-questions_answered_incorrect = 0
-
-#Number of question answered
-number_of_questions_answered = 0
-
+#Game loop
 game_loop = ""
 while game_loop == "":
 
-    user_choice_of_questions = check_how_many_questions("How many questions do you want to answer: ")
+    #Questions answered correct
+    questions_answered_correct = 0
+
+    #Questions answered incorrect
+    questions_answered_incorrect = 0
+
+    #Number of question answered
+    number_of_questions_answered = 0
+
+    #Ask the User how many questions they want to answer if they want to play the
+    #option where they can choose how many questions they want
+    #to answer of if they want to play the continuous question option
+    user_choice_of_questions = check_how_many_questions("There are two options you can choose from for how many question you can answer.\n"
+                                                        "The first is where you choose how many question you want to answer.\n"
+                                                        "To choose this option just simply input an integer value.\n"
+                                                        "The second option is where a continuous number of questions are asked to you.\n"
+                                                        "TO choose the second option press <ENTER>\n"
+                                                        "\nHow many questions do you want to answer: ")
 
     #Asks the User what game mode they want to play (1. Addition 2. Subtraction 3. Multiplication 4. Division)
     game_mode = game_mode_input_checker("What game mode do you want to play (1. Addition 2. Subtraction 3. Multiplication 4. Division)? ")
@@ -142,7 +168,7 @@ while game_loop == "":
         else:
             heading = "Question {} of {}".format(number_of_questions_answered + 1, user_choice_of_questions)
 
-            #Game Mode 1. Addition
+        #Game Mode 1. Addition
         if game_mode == 1:
 
             #Displays to the User the Question Number
@@ -162,6 +188,8 @@ while game_loop == "":
             response = input_checker("What is {} + {}?  ".format(number_1, number_2))
 
             #If the User's response is equal to the answer (Number 1 + Number 2) the User gets the question 'Correct'
+            #The number of questions answered increases by 1
+            #The number of qestions answered correct increases by 1
             if response == answer:
                 result = "Correct"
                 question_outcome = "Question {} | Result: {} | Your Answer: {} | Correct Answer: {}".format(number_of_questions_answered + 1, result, response, answer)
@@ -171,6 +199,8 @@ while game_loop == "":
 
 
             #If the User's response is not equal to the answer (Number 1 + Number 2) the User gets the question 'Incorrect'
+            #The number of questions answered increases by 1
+            #The number of qestions answered correct increases by 1
             else:
                 result = "Incorrect"
                 question_outcome = "Question {} | Result: {} | Your Answer: {} | Correct Answer: {}".format(number_of_questions_answered + 1, result, response, answer)
@@ -178,10 +208,11 @@ while game_loop == "":
                 number_of_questions_answered += 1
                 questions_answered_incorrect += 1
 
-
+            #The question outcome of question is outputed to the User
             print(question_outcome)
 
             #End game if the number of rounds has been played
+            #The number of questions answered is reset back to 0
             if number_of_questions_answered == user_choice_of_questions:
                 number_of_questions_answered = 0
                 break
@@ -230,6 +261,8 @@ while game_loop == "":
 
             #If the User's response is equal to the answer (Number 1 - Number 2/Number 2 - Number 1)
             #The User gets the question 'Correct'
+            #The number of questions answered increases by 1
+            #The number of qestions answered correct increases by 1
             if response == answer:
                 result = "Correct"
                 question_outcome = "Question {} | Result: {} | Your Answer: {} | Correct Answer: {}".format(number_of_questions_answered + 1, result, response, answer)
@@ -237,8 +270,10 @@ while game_loop == "":
                 number_of_questions_answered += 1
                 questions_answered_correct += 1
 
-                #If the User's response is not equal to the answer (Number 1 - Number 2/Number 2 - Number 1)
-                #The User gets the question 'Incorrect'
+            #If the User's response is not equal to the answer (Number 1 - Number 2/Number 2 - Number 1)
+            #The User gets the question 'Incorrect'
+            #The number of questions answered increases by 1
+            #The number of qestions answered correct increases by 1
             else:
                 result = "Incorrect"
                 question_outcome = "Question {} | Result: {} | Your Answer: {} | Correct Answer: {}".format(number_of_questions_answered + 1, result, response, answer)
@@ -246,9 +281,11 @@ while game_loop == "":
                 number_of_questions_answered += 1
                 questions_answered_incorrect += 1
 
+            #The question outcome of question is outputed to the User
             print(question_outcome)
 
             #End game if the number of rounds has been played
+            #The number of questions answered is reset back to 0
             if number_of_questions_answered == user_choice_of_questions:
                 number_of_questions_answered = 0
                 break
@@ -286,6 +323,8 @@ while game_loop == "":
 
             #If the User's response is equal to the answer (Number 1 x Number 2)
             #The User gets the question 'Correct'
+            #The number of questions answered increases by 1
+            #The number of qestions answered correct increases by 1
             if response == answer:
                 result = "Correct"
                 question_outcome = "Question {} | Result: {} | Your Answer: {} | Correct Answer: {}".format(number_of_questions_answered + 1, result, response, answer)
@@ -295,6 +334,8 @@ while game_loop == "":
 
             #If the User's response is not equal to the answer (Number 1 x Number 2)
             #The User gets the question 'Incorrect'
+            #The number of questions answered increases by 1
+            #The number of qestions answered correct increases by 1
             else:
                 result = "Incorrect"
                 question_outcome = "Question {} | Result: {} | Your Answer: {} | Correct Answer: {}".format(number_of_questions_answered + 1, result, response, answer)
@@ -302,9 +343,11 @@ while game_loop == "":
                 number_of_questions_answered += 1
                 questions_answered_incorrect += 1
 
+            #The question outcome of question is outputed to the User
             print(question_outcome)
 
             #End game if the number of rounds has been played
+            #The number of questions answered is reset back to 0
             if number_of_questions_answered == user_choice_of_questions:
                 number_of_questions_answered = 0
                 break
@@ -346,6 +389,8 @@ while game_loop == "":
 
             #If the User's response is equal to the answer (Product answer / Number 1 = Number 2)
             #The User gets the question 'Correct'
+            #The number of questions answered increases by 1
+            #The number of qestions answered correct increases by 1
             if response == answer:
                 result = "Correct"
                 question_outcome = "Question {} | Result: {} | Your Answer: {} | Correct Answer: {}".format(number_of_questions_answered + 1, result, response, answer)
@@ -355,6 +400,8 @@ while game_loop == "":
 
             #If the User's response is not equal to the answer (Product answer / Number 1 = Number 2)
             #The User gets the question 'Incorrect'
+            #The number of questions answered increases by 1
+            #The number of qestions answered correct increases by 1
             else:
                 result = "Incorrect"
                 question_outcome = "Question {} | Result: {} | Your Answer: {} | Correct Answer: {}".format(number_of_questions_answered + 1, result, response, answer)
@@ -362,9 +409,11 @@ while game_loop == "":
                 number_of_questions_answered += 1
                 questions_answered_incorrect += 1
 
+            #The question outcome of question is outputed to the User
             print(question_outcome)
 
             #End game if the number of rounds has been played
+            #The number of questions answered is reset back to 0
             if number_of_questions_answered == user_choice_of_questions:
                 number_of_questions_answered = 0
                 break
@@ -382,6 +431,10 @@ while game_loop == "":
             if game_loop == "xxx":
                 break
 
+    #The User will be asked if they wish to play the game again (<ENTER>) or if they wish to not
+    #play the game again ('xxx')
     game_loop = continue_game("If you wish to play the game again press <ENTER>, if you do not want to play the game again type 'xxx': ")
+    print()
 
+#Thanks the User for playing the game
 print("Thank You for playing the Quiz Quest Game")
